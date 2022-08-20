c469b2fb88ed3615eba1982b87b538f285e2a85e
hoseinbaloch1 <110195595+hoseinbaloch1@users.noreply.github.com>
2022-08-20 23:18:11 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -46,6 +46,7 @@ global.antilink = "false" // To remove group link sharing peoples
 global.upsongvoice = "false" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
+
 global.owner = ['+989339658384']
 global.ownertag = '+989339658384' //your tag number
 global.botname = '☫𝐵𝐴𝐿𝑂𝐶𝐻-𝐸𝐷𝐼𝑇☫' //ur bot name
